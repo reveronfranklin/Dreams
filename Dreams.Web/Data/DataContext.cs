f8b3266248d0ab7471bc83116971eb13ef821b88
Franklyn Reveveron <franklin.reveron@hotmail.com>
2020-02-20 02:12:27 +0100

    Commit inicial

---
+++ Dreams.Web/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Dreams.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dreams.Web.Data
+{
+    public class DataContext:DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ProductEntity> Products { get; set; }
+
+
+    }
+}
---
+++ Dreams.Web/Data/Entities/ProductEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dreams.Web.Data.Entities
+{
+    public class ProductEntity
+    {
+        public int Id { get; set; }
+
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "The {0} field must have {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string  Code { get; set; }
+
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "The {0} field must have {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Descripcion { get; set; }
+
+    }
+}
